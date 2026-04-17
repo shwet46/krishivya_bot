03d00efb64127e3d708b46d7144484d38cbce9cd
Shweta <beherashweta54@gmail.com>
2026-04-17 18:27:04 +0530

    revert to gcp ecosystem

--- llm_processing/__init__.py
+++
@@ -1,7 +0,0 @@
-from llm_processing.llm_service import (
-    WELCOME_MESSAGE,
-    generate_response,
-    parse_reminder_request,
-)
-
-__all__ = ["WELCOME_MESSAGE", "generate_response", "parse_reminder_request"]
